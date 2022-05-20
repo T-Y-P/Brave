10b03e76c94776018e288840cecc7762b8edddfc
pyrrole-ach <77265463+pyrrole-ach@users.noreply.github.com>
2022-05-20 15:53:35 +0800

    Update head.php

--- base/head.php
+++ base/head.php
@@ -15,9 +15,9 @@
 	<link rel="stylesheet" href="/usr/themes/Brave/botui/botui.min.css" />
     <link rel="stylesheet" href="/usr/themes/Brave/botui/botui-theme-default.css" />
 	<link rel="stylesheet" href="<?php $this->options->themeUrl('/base/style.css'); ?>">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/bootstrap/4.6.1/css/bootstrap.min.css">
+	<script src="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/bootstrap/4.6.1/js/bootstrap.min.js"></script>
 	<?php $this->options->头部自定义(); ?>
 </head> 
 <style>
